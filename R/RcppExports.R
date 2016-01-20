03fdc4ae773460c096df56a36b6a570e15d7acc8
Holger <holger.trittenbach@gmail.com>
2016-01-20 18:09:22 +0100

    added quickstart

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -127,6 +127,53 @@ HiCSMatrixC <- function(indexMap, alpha, numRuns) {
     .Call('subcon_HiCSMatrixC', PACKAGE = 'subcon', indexMap, alpha, numRuns)
 }
 
+#' Greedy Maximum Deviation  Heuristic
+#'
+#' Constructs one deviation maximizing subspace for each dimension. The search
+#' is based on the two dimensional deviations. For each dimension r, the
+#' heuristic orders the two dimensional projections [r, s_i] by deviation, with
+#' s_i being the conditional attribute. It then starts with the highest two
+#' dimensional projection as the initial subspace. The next candidate to be
+#' added to that subspace is the dimension that has the second highest
+#' deviation in the two-dimensional projections. If the deviation for r
+#' increases by adding a dimension, the subspace will be increased, else that
+#' dimension is discarded.
+#'
+#' Example:
+#'
+#' reference dimension: 1
+#' 2-dim projections (ordered by deviation):
+#' [1,3] deviation(1|3) = 0.6
+#' [1,2] deviation(1|2) = 0.5
+#' [1,5] deviation(1|5) = 0.4
+#' [1,4] deviation(1|4) = 0.3
+#'
+#' Initial subspace: [1,3]
+#'
+#' add dimension 2
+#' [1,2,3] deviation(1|2,3) = 0.65 -> increases the deviation for the reference dimension
+#'
+#' add dimension 5
+#' [1,2,3,5] deviation(1|2,3) = 0.62 -> decreases the deviation for the reference dimension
+#'
+#' add dimension 5
+#' [1,2,3,5] deviation(1|2,3,5) = 0.62 -> decreases the deviation for the reference dimension
+#'
+#' add dimension 4
+#' [1,2,3,4] deviation(1|2,3,4) = 0.67 -> increases the deviation for the reference dimension
+#'
+#' Output space for reference dimension 1: [1,2,3,4]
+#'
+#'
+#' @param indexMap Index for the data objects if ordered by dimension. Each
+#'   entry of the vector contains the index to the initial data set. The index
+#'   is starting with 1.
+#' @param alpha Percentage of data objects to remain in the subspace slice
+#'   (expected value).
+#' @param numRuns number of random subspace slices used to estimate the
+#'   deviation.
+#' @return List of subspaces for each reference dimension.
+#' @export
 GMD <- function(indexMap, alpha, numRuns) {
     .Call('subcon_GMD', PACKAGE = 'subcon', indexMap, alpha, numRuns)
 }
--- README.md
+++ README.md
@@ -3,7 +3,40 @@ R package for subspace search
 
 This package is an early stage and under development. 
 
-The package contains functions to search for subspaces in high dimensional data bases. The code is based on [HiCS - High Contrast Subspaces for Density Based Outlier Ranking][1].
+## Quick start
+
+Deviation measures 
+
+```
+require(data.table)
+
+#generate sample data
+dt <- data.table(replicate(10,runif(100)))
+
+#pre-process data into index map for efficient deviation calculation
+indexMatrix <- sortedIndexMatrix(dt)
+
+#deviation
+averageDeviationC(indexMap = indexMatrix, subspace = c(1,2), alpha = 0.1, referenceDim = 1, numRuns = 100)
+HiCSContrastC(indexMap = indexMatrix, subspace = c(1,2), alpha = 0.1, referenceDim = 1, numRuns = 100)
+
+#all two dimensional projections
+
+deviationMatrixC(indexMap = indexMatrix, alpha = 0.1, numRuns = 100)
+HiCSMatrixC(indexMap = indexMatrix, alpha = 0.1, numRuns = 100)
+```
+
+Subspace search algorithms
+
+```
+
+#Greedy maximum heuristic
+
+GMD(indexMap = indexMatrix, alpha = 0.1, numRuns = 100)
+
+```
+
+The package contains functions to search for subspaces in high dimensional data bases. The contrast defintion used in the code is based on [HiCS - High Contrast Subspaces for Density Based Outlier Ranking][1].
 
 Implemntation is inspired by the [nim implementation of the HiCS search][2].
 
